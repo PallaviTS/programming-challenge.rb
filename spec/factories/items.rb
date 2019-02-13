08140ff3aae44c5106075767b1e98210022dc682
Pallavi TS <pallavi046@gmail.com>
2019-02-13 22:52:10 +0530

    Added controller and its specs

---
+++ spec/factories/items.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :item do
+    title { Faker::Lorem.word }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'database_cleaner'
-require 'rspec/rails'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -24,8 +25,7 @@ end
 
 RSpec.configure do |config|
 
-  # add `FactoryBot` methods
-  config.include FactoryBot::Syntax::Methods
+
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
@@ -62,4 +62,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
 end
